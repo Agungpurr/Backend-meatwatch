2b3fd4823320dffbddd81c7718df9cdfa80088e7
Agung Purnomo <122212227+Agungpurr@users.noreply.github.com>
2025-06-03 01:42:16 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("✅ Loaded DB config:");
+console.log("Host:", process.env.DB_HOSTS);
+console.log("Port:", process.env.DB_PORT);
+console.log("User:", process.env.DB_USER);
+console.log("DB Name:", process.env.DB_NAME);
+
 async function connectDB() {
   const pool = await mysql.createPool({
-    host: process.env.DB_HOSTS, // pakai DB_HOSTS
+    host: process.env.DB_HOSTS, // ⬅️ Gunakan DB_HOSTS dari Railway
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
