2120b9b8a5f986bf859dc32e0ff22e2970e60dea
Agung Purnomo <122212227+Agungpurr@users.noreply.github.com>
2025-06-03 01:26:18 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 async function connectDB() {
   const pool = await mysql.createPool({
-    host: process.env.DB_HOSTS,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
